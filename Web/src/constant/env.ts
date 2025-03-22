a319af4a0110eaeee5e447fe4a909c61ea9fc752
Phat(BE) <phatnguyen@everfit.io>
2025-03-22 21:18:40 +0700

    feat(web note): init source web

--- Web/src/constant/env.ts
+++ Web/src/constant/env.ts
@@ -1,6 +1,7 @@
-export const isProd = process.env.NODE_ENV === 'production';
-export const isLocal = process.env.NODE_ENV === 'development';
+export const isProd = process.env.NODE_ENV === "production";
+export const isLocal = process.env.NODE_ENV === "development";
 
-export const showLogger = isLocal
-  ? true
-  : process.env.NEXT_PUBLIC_SHOW_LOGGER === 'true' ?? false;
+export const showLogger = isLocal ? true : false;
+
+export const API_URL =
+  process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:3344";
