659ebed7c4aa5422fae0c71c421459c7b1555bd6
Phat(BE) <phatnguyen@everfit.io>
2025-03-23 22:22:57 +0700

    feat: save slug by using title

--- src/use-cases/note/note-factory.service.ts
+++ src/use-cases/note/note-factory.service.ts
@@ -1,14 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { CreateNoteDto, UpdateNoteDto } from '../../core/dtos';
 import { Note } from 'src/frameworks/data-services/mongo/model';
-
 @Injectable()
 export class NoteFactoryService {
   createNewNote(createNoteDto: CreateNoteDto) {
     const newNote = new Note();
     newNote.title = createNoteDto.title;
     newNote.content = createNoteDto.content;
-    newNote.slug = createNoteDto.slug;
+    newNote.slug = createNoteDto.title.toLowerCase().trim().replace(/ /g, '-');
     newNote.created_by = 'system';
     return newNote;
   }
@@ -17,7 +16,7 @@ export class NoteFactoryService {
     const updatedNote = new Note();
     updatedNote.title = updateNoteDto.title;
     updatedNote.content = updateNoteDto.content;
-    updatedNote.slug = updateNoteDto.slug;
+    updatedNote.slug = updateNoteDto.title.toLowerCase().trim().replace(/ /g, '-');
 
     return updatedNote;
   }
