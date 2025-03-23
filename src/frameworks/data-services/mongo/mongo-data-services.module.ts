fe59fb081ad5268ab16cf3e4f4fc3fd344c63739
Phat(BE) <phatnguyen@everfit.io>
2025-03-24 03:05:54 +0700

    build: fix error build heruko

--- src/frameworks/data-services/mongo/mongo-data-services.module.ts
+++ src/frameworks/data-services/mongo/mongo-data-services.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { IDataServices } from '../../../core';
-import { DATA_BASE_CONFIGURATION } from '../../../configuration';
 import { Account, AccountSchema, Note, NoteSchema } from './model';
 import { MongoDataServices } from './mongo-data-services.service';
 
@@ -11,7 +10,7 @@ import { MongoDataServices } from './mongo-data-services.service';
       { name: Account.name, schema: AccountSchema },
       { name: Note.name, schema: NoteSchema },
     ]),
-    MongooseModule.forRoot(DATA_BASE_CONFIGURATION.mongoConnectionString),
+    MongooseModule.forRoot(process.env.MONGO_CONNECTION_STRING),
   ],
   providers: [
     {
