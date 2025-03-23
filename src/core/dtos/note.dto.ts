83f50f06106ddd75095717938e6e745575a0fb05
Phat(BE) <phatnguyen@everfit.io>
2025-03-23 22:22:26 +0700

    doc: update swager info to dto

--- src/core/dtos/note.dto.ts
+++ src/core/dtos/note.dto.ts
@@ -1,17 +1,29 @@
-import { IsString, IsNotEmpty, IsDate } from 'class-validator';
+import { IsString, IsOptional, IsDefined } from 'class-validator';
 import { PartialType } from '@nestjs/mapped-types';
-
+import { ApiProperty } from '@nestjs/swagger';
 export class CreateNoteDto {
+  @ApiProperty({
+    description: 'The title of the note',
+    example: 'My first note',
+  })
   @IsString()
-  @IsNotEmpty()
+  @IsDefined()
   title: string;
 
+  @ApiProperty({
+    description: 'The content of the note',
+    example: 'This is the content of my first note',
+  })
   @IsString()
-  @IsNotEmpty()
+  @IsOptional()
   content: string;
 
+  @ApiProperty({
+    description: 'The slug of the note',
+    example: 'my-first-note',
+  })
   @IsString()
-  @IsNotEmpty()
+  @IsOptional()
   slug: string;
 
   // @IsString()
@@ -20,3 +32,29 @@ export class CreateNoteDto {
 }
 
 export class UpdateNoteDto extends PartialType(CreateNoteDto) {}
+
+export class GetAllNotesDto {
+  @ApiProperty({
+    description: 'The page number',
+    example: 1,
+  })
+  page: number;
+
+  @ApiProperty({
+    description: 'The limit of the notes',
+    example: 10,
+  })
+  limit: number;
+
+  @ApiProperty({
+    description: 'The query of the notes',
+    example: 'my-first-note',
+  })
+  q: string;
+
+  @ApiProperty({
+    description: 'The sort of the notes',
+    example: 'created_at',
+  })
+  sort: string;
+}
