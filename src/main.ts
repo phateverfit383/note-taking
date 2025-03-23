d994cf83f6699af5c3f35828e618203093ec2a5f
Phat(BE) <phatnguyen@everfit.io>
2025-03-23 21:56:45 +0700

    build: add allow cors

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,12 @@ async function bootstrap() {
   const options = new DocumentBuilder().addBearerAuth(undefined, 'x-access-token').build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('docs', app, document);
+  app.enableCors({
+    origin: '*',
+    allowedHeaders: ['x-access-token', 'Content-Type'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    credentials: true,
+  });
   await app.listen(process.env.PORT || 3040);
   console.log(`Server is running on: http://localhost:${process.env.PORT || 3040}`);
 }
